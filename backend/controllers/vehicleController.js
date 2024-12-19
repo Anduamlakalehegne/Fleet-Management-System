8b57a6f1fbd131d754a43c338e5b50c6584481a3
Anduamlak Alehegne <111687327+Anduamlakalehegne@users.noreply.github.com>
2024-12-19 22:36:30 +0300

    Update vehicleController.js

--- backend/controllers/vehicleController.js
+++ backend/controllers/vehicleController.js
@@ -4,28 +4,32 @@ const { BACKEND_URL } = require('../config');
 
 exports.getAllVehicles = async (req, res, next) => {
   try {
-    const response = await axios.get(`${BACKEND_URL}/api/vehicles`);
-    res.json(response.data);
-  } catch (error) {
-    next(error);
+    const vehicles = await Vehicle.find();
+    res.json(vehicles);
+  } catch (err) {
+    handleError(err, res);
   }
 };
 
 exports.createVehicle = async (req, res, next) => {
   try {
-    const response = await axios.post(`${BACKEND_URL}/api/vehicles`, req.body);
-    res.status(201).json(response.data);
-  } catch (error) {
-    next(error);
+    const newVehicle = new Vehicle(req.body);
+    const savedVehicle = await newVehicle.save();
+    res.status(201).json(savedVehicle);
+  } catch (err) {
+    handleError(err, res);
   }
 };
 
 exports.updateVehicle = async (req, res, next) => {
   try {
-    const response = await axios.put(`${BACKEND_URL}/api/vehicles/${req.params.id}`, req.body);
-    res.json(response.data);
-  } catch (error) {
-    next(error);
+    const updatedVehicle = await Vehicle.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!updatedVehicle) {
+      return res.status(404).json({ message: 'Vehicle not found' });
+    }
+    res.json(updatedVehicle);
+  } catch (err) {
+    handleError(err, res);
   }
 };
 
